2089ba243d9a397e744469851f0ca8ec0eb24ffc
Laura <luaana788@gmail.com>
2019-11-07 18:08:48 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const seccionesPagina = new fullpage('#fullpage', {
+    autoScrolling: true,
+	fitToSection: false,
+    fitToSectionDelay: 300,
+    easing: 'easeInOutCubic',
+    scrollingSpeed: 700,
+    css3: true,
+    easingcss3: 'ease-out',
+	loopBottom: false
+});
